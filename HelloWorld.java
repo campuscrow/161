29150d3f47c38d99d6358cbbb55bced389a2fdc9
campuscrow <campuscrow@users.noreply.github.com>
2015-08-31 15:29:18 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
-# Peter Stanley Lab 01
+// Peter Stanley Lab 01
 
+// Prints Hello, World! when run (I hope)
 private class HelloWorld { 
   private static void main(String[] args) { 
     System.out.println("Hello, World!");
